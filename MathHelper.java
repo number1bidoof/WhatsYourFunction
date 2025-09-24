910fa7b6901d030e5ac6afc2177ff0256f6fda46
anabdalla1 <anabdalla1@my.waketech.edu>
2025-09-24 10:10:21 -0400

    added comments

--- MathHelper.java
+++ MathHelper.java
@@ -2,22 +2,23 @@ public class MathHelper {
     public static int calculateSum(int i, int j) {
         return i + j;
     }
-    public static int findMin(int z, int y) {
+    public static int findMin(int z, int y) { // finds the minimum manually
         int miniNum;
-        if (z > y){
+        if (z > y){ // if it is greater, then it prints the lowest integer
             miniNum = y;
-        } else {
+        } else { // if the first condition is not true
             miniNum = z;
         }
-        return miniNum;
+        return miniNum; // returns the number
     }
     public static void printResult(int x){
         System.out.println("The result is... " + x);
     }
 
     public static void main(String[] args) {
-        int sum = calculateSum(15, 25);
-        int min = findMin(10, 20);
+        int sum = calculateSum(15, 25); // finds the sum of 15 and 25
+        int min = findMin(10, 20); // finds the minimum of 10 and 15
+        // formats output
         printResult(sum);
         printResult(min);
     }
--- VariableScope.java
+++ VariableScope.java
@@ -1,10 +1,10 @@
 public class VariableScope {
     public static int tryToModify(int o){
-        return(o + 10);
+        return(o + 10); // returns the integer + 10
     }
     public static int[] processArray(int[] p){
-        for(int i = 0; i < p.length; i++){
-            p[i] *= 2;
+        for(int i = 0; i < p.length; i++){ // enumerates through each element in the array
+            p[i] *= 2; // multiples each element by 2
         }
         return p;
     }
@@ -12,17 +12,18 @@ public class VariableScope {
         int x = 100;
         for(int i = 0; i < 3; i++){
             int y = i * 10;
-            System.out.println(x);
-            System.out.println(y);
+            System.out.println(x); // prints out x in the method scope
+            System.out.println(y); // prints out y inside the for loop scope
         }
         System.out.println(x);
-        ///System.out.println(y);
+        ///System.out.println(y); causes an error
     }
         public static void main(String[] args) {
             int number = 5;
             System.out.println("Before method call: " + number);
             number = tryToModify(number);
             System.out.println("After method call: " + number);
+
             int[] arr = {1, 2, 3, 4, 5};
             System.out.print("Before: ");
             for(int val : arr) System.out.print(val + " ");
