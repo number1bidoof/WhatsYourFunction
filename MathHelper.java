4a9fd961d7fb88e5b6e1ea8d8e7b7f960ab969bc
anabdalla1 <anabdalla1@my.waketech.edu>
2025-09-24 09:12:29 -0400

    passed precalc with an A

---
+++ MathHelper.java
@@ -0,0 +1,24 @@
+public class MathHelper {
+    public static int calculateSum(int i, int j) {
+        return i + j;
+    }
+    public static int findMin(int z, int y) {
+        int miniNum;
+        if (z > y){
+            miniNum = y;
+        } else {
+            miniNum = z;
+        }
+        return miniNum;
+    }
+    public static void printResult(int x){
+        System.out.println("The result is: " + x);
+    }
+
+    public static void main(String[] args) {
+        int sum = calculateSum(15, 25);
+        int min = findMin(10, 20);
+        printResult(sum);
+        printResult(min);
+    }
+}
